f78c8ef4b64694b1fbeea0c573e7a69190bdd6d9
Shrut Patel <shrut.patel@ba.com>
2023-03-08 13:23:27 +0000

    PART 3

--- dbtvault/models/raw_vault/sats/sat_parent_issue.sql
+++ dbtvault/models/raw_vault/sats/sat_parent_issue.sql
@@ -1,8 +1,7 @@
 {%- set source_model = "v_stg_issue" -%}
-{%- set src_pk = "ISSUE_HASH_KEY" -%}
-{%- set src_hashdiff = "ISSUE_HASHDIFF" -%}
-{%- set src_payload = ["ISSUE_ID", "PROJECT_ID", "SUMMARY", "DESCRIPTION", "PRIORITY", "CREATED", "UPDATED", "STATUS", "PROGRESS", "ISSUE_TYPE", "LABELS", "VOTES",
-                       "DUE_DATE", "TIME_ORIGINAL_ESTIMATE", "TIME_REMAINING_ESTIMATE", "TIME_SPENT"] -%}
+{%- set src_pk = "PARENT_ISSUE_HASH_KEY" -%}
+{%- set src_hashdiff = "PARENT_ISSUE_HASHDIFF" -%}
+{%- set src_payload = ["RELATION", "PARENT_TYPE"] -%}
 {%- set src_eff = "EFFECTIVE_FROM" -%}
 {%- set src_ldts = "LOAD_DATE" -%}
 {%- set src_source = "RECORD_SOURCE" -%}
